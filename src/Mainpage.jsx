306cff9a183725eea07c098c7c2326887e9a5894
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-03-10 19:06:11 +0530

    added the dotenv for the secret api key

--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "./slices/authslice";
 const Mainpage = () => {
-    const API_URL = import.meta.env.API_SECRET_KEY;
+    const API_URL = import.meta.env.VITE_SECRET_KEY;
     const dispatch=useDispatch();
     const [answerdelay, setdelay] = useState(false);
     const navigate=useNavigate();
