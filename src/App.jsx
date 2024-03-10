baee2b486a1536e8f5578ee0bbe8fbdc42138adb
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-03-10 18:41:18 +0530

    changed browserrouter to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Mainpage from "./Mainpage";
 import Login from "./Login";
 
-
 function App() {
-  return(
-    <BrowserRouter>
-    <Routes>
-      <Route  path="/" element={<Login/>}/>
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
 
-      <Route path="/home" element={<Mainpage/>}/>
-    </Routes>
-    </BrowserRouter>
-  )
+        <Route path="/home" element={<Mainpage />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
